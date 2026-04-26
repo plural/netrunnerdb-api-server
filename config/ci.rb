8ab7f63bc26fef6259675f02d0aa5caab0937edb
Jason Gessner <jason@multiply.org>
2026-04-26 18:03:55 +0000

    Reformat config/ci.rb

--- config/ci.rb
+++ config/ci.rb
@@ -4,15 +4,11 @@
 
 CI.run do
   step 'Setup', 'bin/setup --skip-server'
-
   step 'Style: Ruby', 'bin/rubocop'
-
   step 'Security: Gem audit', 'bin/bundler-audit'
   step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
   step 'Tests: Rails', 'bin/rails test'
   step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
-
-  # Optional: Run system tests
   step 'Tests: System', 'bundle exec rspec --fail-fast'
 
   # Optional: set a green GitHub commit status to unblock PR merge.
